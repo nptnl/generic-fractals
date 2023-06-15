b248d8697c3320c43a2eb9f00e9990ac1e176cbd
Neptunal <110062414+nptnl@users.noreply.github.com>
2023-06-15 17:54:27 -0400

    easier tweaking
    im gonna multithread this stuff

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,57 @@ use basemath::*;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::thread;
 
 static HEX_GS: [char; 16] = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',];
 
 fn main() {
-    ispace(Comp::new(0.905, 0.45), 8192, 2.0, 128);
+    let seed: Comp = Comp { r: 0.0, i: 0.0 };
+    let topleft: Comp = Comp { r: -1.5, i: 1.4 };
+    let bottomright: Comp = Comp { r: 3.0, i: -0.4 };
+    let bound: f64 = 2.0;
+    let bound = bound * bound;
+    let size: u32 = 1024;
+    let iterate: usize = 16;
+
+    ispace(seed, topleft, bottomright, bound, size, iterate);
 }
 
 #[allow(dead_code)]
-fn ispace(c: Comp, size: u32, planesize: f64, iterate: usize) {
+fn ispace(c: Comp, topleft: Comp, bottomright: Comp, bound: f64, size: u32, iterate: usize) {
 
-    let path = Path::new("./plots/current.npxl");
+    let path = Path::new("./plots/icurrent.npxl");
     let mut file = File::create(&path).unwrap();
     let first = format!("{} {}\n", size*2, size*2) + "16 1\n";
     file.write_all(first.as_bytes()).expect("cannot write header");
     
-    let formula = |z: Comp, c: Comp| z*z*z*z*z*z + c;
+    let formula = |z: Comp, c: Comp| z*z + c;
 
-    let step: f64 = planesize / size as f64;
-    let mut crnt: Comp = Comp::new(-planesize + 0.5*step, planesize - 0.5*step);
+    let rstep: f64 = (bottomright.r - topleft.r) / size as f64;
+    let istep: f64 = (topleft.i - bottomright.i) / size as f64;
+    let mut crnt: Comp = topleft + Comp::new(0.5*rstep, -0.5*istep);
     let mut counter: usize;
     let mut valstring: String;
     let mut z: Comp;
 
     for _ in 0..size*2 {
         valstring = String::new();
-        crnt.r = -planesize + 0.5*step;
+        crnt.r = topleft.r + 0.5*rstep;
         for _ in 0..size*2 {
             counter = 0;
             z = crnt;
             loop {
                 if counter == iterate { counter = 0; break }
-                if z.r*z.r + z.i*z.i > planesize * planesize { break }
+                if z.r*z.r + z.i*z.i > bound * bound { break }
                 z = formula(z, c);
                 counter += 1;
             }
             valstring = format!("{valstring}{}", HEX_GS[counter * 16 / iterate]);
-            crnt.r += step;
+            crnt.r += rstep;
         }
         valstring += "\n";
         file.write_all(valstring.as_bytes()).expect("cannot write line");
-        crnt.i -= step;
+        crnt.i -= istep;
     }
 
 }
@@ -50,12 +60,12 @@ fn ispace(c: Comp, size: u32, planesize: f64, iterate: usize) {
 #[allow(dead_code)]
 fn pspace(seed: Comp, size: u32, planesize: f64, iterate: usize) {
 
-    let path = Path::new("./plots/current.npxl");
+    let path = Path::new("./plots/pcurrent.npxl");
     let mut file = File::create(&path).unwrap();
     let first = format!("{} {}\n", size*2, size*2) + "16 1\n";
     file.write_all(first.as_bytes()).expect("cannot write header");
 
-    let formula = |z: Comp, c: Comp| sin(z*z) + c;
+    let formula = |z: Comp, c: Comp| z*z + c;
 
     let step: f64 = planesize / size as f64;
     let mut crnt: Comp = Comp::new(-planesize + 0.5*step, planesize - 0.5*step);
