285a67fb128701dadf88ba52ef1674ac1a1201be
Neptunal <110062414+nptnl@users.noreply.github.com>
2023-06-17 20:11:00 -0400

    32 colors
    images look way better, working on base64 but it may take longer

---
+++ readme.md
@@ -0,0 +1,5 @@
+# generic fractal generation
+
+use any function you can come up with to generic input-space and parameter-space fractals
+
+math capabilities provided by my own math library, [nptnl/basemath](https://github.com/nptnl/basemath)
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,23 @@ use std::io::prelude::*;
 use std::path::Path;
 use std::thread;
 
-static HEX_GS: [char; 16] = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',];
+#[allow(dead_code)]
+static HEX_GS: [char; 16] = [
+    '0','1','2','3','4','5','6','7','8','9',
+    'a','b','c','d','e','f'];
+static LOWER_GS: [char; 32] = [
+    '0','1','2','3','4','5','6','7','8','9',
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+];
 
 fn main() {
-    let seed: Comp = Comp { r: 0.0, i: 0.0 };
-    let topleft: Comp = Comp { r: -2.0, i: 2.0 };
-    let bottomright: Comp = Comp { r: 2.0, i: -2.0 };
-    let bound: f64 = 2.0;
-    let width: u32 = 16384;
-    let height: u32 = 16384;
-    let iterate: usize = 128;
+    let seed: Comp = Comp { r: -0.0, i: 0.0 };
+    let topleft: Comp = Comp { r: -8.0, i: 8.0 };
+    let bottomright: Comp = Comp { r: 8.0, i: -8.0 };
+    let bound: f64 = 20.0;
+    let width: u32 = 512;
+    let height: u32 = 512;
+    let iterate: usize = 32;
     let threads: u32 = 8;
 
     multi_p(seed, topleft, bottomright, bound, width, height, iterate, threads);
@@ -31,10 +38,10 @@ fn multi_i(
     threads: u32,
 ) {
     let mut header = File::create("./plots/build/0.npxl").expect("cannot create cur file");
-    let first = format!("{} {}\n", width, height) + "16 1\n";
+    let first = format!("{} {}\n", width, height) + "32 1\n";
     header.write_all(first.as_bytes()).expect("cannot write header");
 
-    let separation: f64 = (topleft.i - bottomright.i) * 0.125;
+    let separation: f64 = (topleft.i - bottomright.i) / threads as f64;
     let mut loc_tl: Comp = topleft;
     let mut loc_br: Comp = Comp { r: bottomright.r, i: topleft.i - separation};
     let mut allthr: Vec<_> = Vec::new();
@@ -60,11 +67,11 @@ fn multi_p(
     iterate: usize,
     threads: u32,
 ) {
-    let mut header = File::create("./plots/build/0.npxl").expect("cannot create cur file");
-    let first = format!("{} {}\n", width, height) + "16 1\n";
+    let mut header = File::create("./plots/build/0.npxl").expect("cannot create header file");
+    let first = format!("{} {}\n", width, height) + "32 1\n";
     header.write_all(first.as_bytes()).expect("cannot write header");
 
-    let separation: f64 = (topleft.i - bottomright.i) * 0.125;
+    let separation: f64 = (topleft.i - bottomright.i) / threads as f64;
     let mut loc_tl: Comp = topleft;
     let mut loc_br: Comp = Comp { r: bottomright.r, i: topleft.i - separation};
     let mut allthr: Vec<_> = Vec::new();
@@ -95,7 +102,7 @@ fn ispace(
     let path = Path::new(name.as_str());
     let mut file = File::create(&path).unwrap();
     
-    let formula = |z: Comp, c: Comp| z*z + c;
+    let formula = |z: Comp, c: Comp| tanh(z) + c;
 
     let rstep: f64 = (bottomright.r - topleft.r) / width as f64;
     let istep: f64 = (topleft.i - bottomright.i) / height as f64;
@@ -112,11 +119,11 @@ fn ispace(
             z = crnt;
             loop {
                 if counter == iterate { counter = 0; break }
-                if z.r*z.r + z.i*z.i > bound * bound { break }
+                if z.mag_square() > bound * bound { break }
                 z = formula(z, par);
                 counter += 1;
             }
-            valstring = format!("{valstring}{}", HEX_GS[counter * 16 / iterate]);
+            valstring = format!("{valstring}{}", LOWER_GS[counter * 32 / iterate]);
             crnt.r += rstep;
         }
         valstring += "\n";
@@ -140,7 +147,7 @@ fn pspace(
     let path = Path::new(name.as_str());
     let mut file = File::create(&path).unwrap();
     
-    let formula = |z: Comp, c: Comp| z*z + c;
+    let formula = |z: Comp, c: Comp| sin(z) + z*z*z + c;
     
     let rstep: f64 = (bottomright.r - topleft.r) / width as f64;
     let istep: f64 = (topleft.i - bottomright.i) / height as f64;
@@ -148,7 +155,6 @@ fn pspace(
     let mut counter: usize;
     let mut valstring: String;
     let mut z: Comp;
-    let mut c: Comp;
 
     for _ in 0..height {
         valstring = String::new();
@@ -156,14 +162,13 @@ fn pspace(
         for _ in 0..width {
             counter = 0;
             z = seed;
-            c = crnt;
             loop {
                 if counter == iterate { counter = 0; break }
-                if z.r*z.r + z.i*z.i > bound * bound { break }
-                z = formula(z, c);
+                if z.mag_square() > bound * bound { break }
+                z = formula(z, crnt);
                 counter += 1;
             }
-            valstring = format!("{valstring}{}", HEX_GS[counter * 16 / iterate]);
+            valstring = format!("{valstring}{}", LOWER_GS[counter * 32 / iterate]);
             crnt.r += rstep;
         }
         valstring += "\n";
